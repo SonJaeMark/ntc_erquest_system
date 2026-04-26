1b399c31b2862b3ea62109d2415e36fc34ba1183
SonJaeMark <424003305@ntc.edu.ph>
2026-04-26 13:03:27 +0800

    first commit

---
+++ src/main/java/com/github/sonjaemark/ntc_erquest_system/NtcErquestSystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.sonjaemark.ntc_erquest_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtcErquestSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtcErquestSystemApplication.class, args);
+	}
+
+}
