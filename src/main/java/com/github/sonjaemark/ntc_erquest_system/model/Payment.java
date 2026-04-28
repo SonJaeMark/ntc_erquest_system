20b05458c1c18ccf0e38628c210e3edd7a12be87
senpaikazuya02-arch <senpaikazuya02@gmail.com>
2026-04-29 03:20:15 +0800

    add Payment model and PaymentMethod enum

--- src/main/java/com/github/sonjaemark/ntc_erquest_system/model/Payment.java
+++ src/main/java/com/github/sonjaemark/ntc_erquest_system/model/Payment.java
@@ -2,11 +2,18 @@ package com.github.sonjaemark.ntc_erquest_system.model;
 
 import java.time.LocalDateTime;
 
+import com.github.sonjaemark.ntc_erquest_system.model.enums.PaymentMethod;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,13 +30,19 @@ public class Payment {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private double amount;
-    private LocalDateTime paid_at;
+    private LocalDateTime paidAt;
+
     @Enumerated(EnumType.STRING)
     private PaymentMethod paymentMethod;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "documentRequest_id", nullable = false)
+    private DocumentRequest documentrequest;
+     
     @PrePersist
     void onCreate() {
-        this.paid_at = LocalDateTime.now();
+        this.paidAt = LocalDateTime.now();
     }
 }
