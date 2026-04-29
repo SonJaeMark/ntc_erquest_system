74014e9effa9c371efb7af5a2c4d13a5bf3950d2
senpaikazuya02-arch <senpaikazuya02@gmail.com>
2026-04-29 12:21:14 +0000

    add RequestLogs model and repository; update NtcErquestSystemApplication to log requests

--- src/main/java/com/github/sonjaemark/ntc_erquest_system/repository/RequestLogsRepository.java
+++ src/main/java/com/github/sonjaemark/ntc_erquest_system/repository/RequestLogsRepository.java
@@ -1,9 +1,12 @@
 package com.github.sonjaemark.ntc_erquest_system.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.sonjaemark.ntc_erquest_system.model.RequestLogs;
 
 public interface RequestLogsRepository extends JpaRepository<RequestLogs, Long>{
-
+   
+    List<RequestLogs> findByDocumentRequest_Student_Id(Long StudentId);
 }
